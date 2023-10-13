dc6e8bdc23b7b143fb840cabe0cd9753949ba143
mshameer260@gmail.com <mshameer260@gmail.com>
2023-10-13 16:52:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const server = express();
+const UserModel = require('./model/User');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/crud');
+    console.log("db connected");   
+}
+
+server.use(cors());
+server.use(bodyparser.json());
+server.use(express.json());
+
+server.get('/read', (req, res) => {
+    UserModel.find({})
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+server.get('/update/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({ _id: id })
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+server.post('/create', (req, res) => {
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+server.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({ _id: id },
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+server.delete('/deleteUser/:id',(req,res)=>{
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id:id})
+    .then(users => res.json(res))
+    .catch(err => res.json(err))
+
+})
+server.listen(1527, () => {
+    console.log("server is running");
+})
