cb8acaf300569aca75e9f44d95d56e0079d28afd
Mohammed Shameer <118889168+MohammedShameer1532@users.noreply.github.com>
2023-10-14 21:33:02 +0530

    Update Read.jsx

--- mernstack-frontend/src/component/Read.jsx
+++ mernstack-frontend/src/component/Read.jsx
@@ -7,13 +7,13 @@ const Read = () => {
   const [user, setUser] = useState([]);
 
   useEffect(() => {
-    axios.get('https://full-stack-backend-jet.vercel.app/read')
+    axios.get('https://backend-eight-black.vercel.app/read')
       .then(result => setUser(result.data))
       .catch(err => console.log(err))
   }, [])
 
   const handleDelete = (id) => {
-    axios.delete(`https://full-stack-backend-jet.vercel.app/deleteUser/${id}`)
+    axios.delete(`https://backend-eight-black.vercel.app/deleteUser/${id}`)
       .then(result => {
         console.log(result)
         window.location.reload()
